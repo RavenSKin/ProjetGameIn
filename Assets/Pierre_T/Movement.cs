2a0a78297e5ee812528216e8f16860a601cabfae
RavenSKin <59887798+RavenSKin@users.noreply.github.com>
2021-05-21 16:16:56 +0200

    Animations des personnages

--- Assets/Pierre_T/Movement.cs
+++ Assets/Pierre_T/Movement.cs
@@ -4,26 +4,26 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    Vector2 CharacterPos;
-    
-    public Rigidbody2D rb2D;
-    public float moveSpeed ;
-    public Transform MovePoint;
-    public LayerMask Object;
-    public float moveDistance;
-  public  bool IsMovingHorizontal;
+
+    public  Animator chara_Anim;
+    public  Rigidbody2D rb2D;
+    public  float moveSpeed ;
+    public  bool IsMovingHorizontal;
     public  bool IsMovingVertical;
-    bool StopMoving;
-   public bool IsMoving;
-    public Vector3 LastMovement;
+    public  bool IsMoving;
+           
     float moveX;
     float moveY;
+    Vector2 CharacterPos;
  
     // Update is called once per frame
     void Update()
     {
+        chara_Anim.SetFloat("X", moveX);
+        chara_Anim.SetFloat("Y", moveY);
         moveX = 0f;
         moveY = 0f;
+        IdleSelector();
         if(moveX==0f||moveY == 0f)
         {
             IsMoving = false;
@@ -39,12 +39,14 @@ public class Movement : MonoBehaviour
             if (Input.GetKey(KeyCode.Z))
             {
                 moveY = 1;
+                chara_Anim.SetBool("IsWalking", true);
                 IsMovingVertical = true;
                 IsMovingHorizontal = false;
             }
             if (Input.GetKey(KeyCode.S))
             {
                 moveY = -1;
+                chara_Anim.SetBool("IsWalking", true);
                 IsMovingVertical = true;
                 IsMovingHorizontal = false;
             }
@@ -54,12 +56,14 @@ public class Movement : MonoBehaviour
             if (Input.GetKey(KeyCode.Q))
             {
                 moveX = -1;
+                chara_Anim.SetBool("IsWalking", true);
                 IsMovingHorizontal = true;
                 IsMovingVertical = false;
             }
             if (Input.GetKey(KeyCode.D))
             {
                 moveX = 1;
+                chara_Anim.SetBool("IsWalking", true);
                 IsMovingHorizontal = true;
                 IsMovingVertical = false;
             }
@@ -69,6 +73,32 @@ public class Movement : MonoBehaviour
     }
 
    
- 
+    void IdleSelector()
+    {
+        if (Input.GetKeyUp(KeyCode.Z))
+        {
+            chara_Anim.SetBool("IsWalking", false);
+            chara_Anim.SetFloat("LastX", 0);
+            chara_Anim.SetFloat("LastY", 1);
+        }
+        if (Input.GetKeyUp(KeyCode.Q))
+        {
+            chara_Anim.SetBool("IsWalking", false);
+            chara_Anim.SetFloat("LastX", -1);
+            chara_Anim.SetFloat("LastY", 0);
+        }
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            chara_Anim.SetBool("IsWalking", false);
+            chara_Anim.SetFloat("LastX", 0);
+            chara_Anim.SetFloat("LastY", -1);
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            chara_Anim.SetBool("IsWalking", false);
+            chara_Anim.SetFloat("LastX", 1);
+            chara_Anim.SetFloat("LastY", 0);
+        }
+    }
     
 }
